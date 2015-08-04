3275d521e6ac62220d287581a5ba56a31b196ade
Noelle Daley <adriannenoelle@gmail.com>
2015-08-04 16:57:44 -0700

    scoping find apartments route

--- server.py
+++ server.py
@@ -7,7 +7,42 @@ app.secret_key = "hello"
 app.jinja_env.undefined = StrictUndefined
 
 
+######### Routes #########
 
+@app.route('/')
+def home():
+    """Return home page."""
+    pass
+
+
+@app.route('find-apartments')
+def find_apartments():
+    """
+    Query database for posts within the user-specified distance.
+
+    Returns list of apartment objects.
+    """
+
+    # max_distance = value from search form
+    # TODO: convert address to lat/long
+
+    # Gather list of tuples w/ ids, lat & longs
+    # all_apts = db.session.query(Posting.post_id, Posting.latitude, Posting.longitude).all()
+
+    # Iterate through tuples, checking if any fit the latitude & longitude criteria.
+    # If they do post id to list.
+    apt_ids = []
+    for id, lat, long in all_apts:
+        if x < lat y and x2 < lon < y2:
+            apt_ids.append(id)
+
+    # Grab all objects in user_apts from database.
+        # Eventually, all items on this list will be plotted on the map.
+    matching_apts = Posting.query.filter(Posting.id.in_([apt_ids]))
+
+    return matching_apts
+
+    
 if __name__ == '__main__':
     app.debug = True
     app.run()
