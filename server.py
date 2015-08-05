2b1eb63247b9c9c21438f2aec58c2f4fa4470de4
Noelle Daley <adriannenoelle@gmail.com>
2015-08-05 15:09:30 -0700

    passing lat lon from form to find_apts

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from model import Posting
+from model import Posting, db
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/find-apartments')
+@app.route('/search-apartments')
 def find_apartments():
     """
     Query database for posts within the user-specified distance.
@@ -25,21 +25,24 @@ def find_apartments():
     """
 
     MILES_TO_DEGREES = 69
-    # max_distance = request.args.get('distance')
-    # dist_degrees = max_distance / MILES_TO_DEGREES
+    max_distance = int(request.args.get('distance'))
+    print max_distance, type(max_distance)
 
-
-    # TODO: convert address to lat/long
-    origin_lat = request.args.get('lat')
-    origin_long = request.args.get('lon')
-    # hard code lat/long
-    # change form to take in lat/long
-    # once that works, switch to address
-
-    # Gather list of tuples w/ ids, lat & longs
-    _QUERY = "SELECT post_id, latitude, longitude FROM postings WHERE SQRT(SQUARE(latitude - ?)) + (SQUARE(longitude - ?)) ) < ?"
-
-    db.session.execute(_QUERY(origin_lat, origin_long, dist_degrees))
+    dist_degrees = max_distance / MILES_TO_DEGREES
+    #
+    #
+    # # TODO: convert address to lat/long
+    #
+    # origin_lat = request.args.get('lat')
+    # origin_long = request.args.get('lon')
+    # # hard code lat/long
+    # # change form to take in lat/long
+    # # once that works, switch to address
+    #
+    # # Gather list of tuples w/ ids, lat & longs
+    # _QUERY = "SELECT post_id, latitude, longitude FROM postings WHERE SQRT(SQUARE(latitude - ?)) + (SQUARE(longitude - ?)) ) < ?"
+    #
+    # db.session.execute(_QUERY(origin_lat, origin_long, dist_degrees))
 
     # all_apts = db.session.query(Posting.post_id, Posting.latitude, Posting.longitude).all()
 
@@ -54,10 +57,13 @@ def find_apartments():
     #     # Eventually, all items on this list will be plotted on the map.
     # matching_apts = Posting.query.filter(Posting.id.in_([apt_ids]))
     #
-    return render_template(matching_apts=matching_apts)
+    return render_template("apts.html", matching_apts=matching_apts)
 
 
-@app.route
+@app.route('/display-apartments')
+def display_apartments():
+    pass
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
