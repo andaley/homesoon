759bca356d9c3a930816d1cea1434f39bddae196
Noelle Daley <adriannenoelle@gmail.com>
2015-08-10 14:21:56 -0700

    added pseudocode

--- static/directions.js
+++ static/directions.js
@@ -3,7 +3,7 @@ google.maps.event.addListener(marker, 'click', calculateDistance)
 
 function calculateDistance() {
   // when user clicks, send server apt ID and calculate distance
-  $.get('/calculate-distance', apt_id, function(distance) {
+  $.get('/calculate-distance.json', apt_id, function(distance) {
     // return json object containing distance
     // add distance to infoWindow
     $('.window-content').append(distance)
