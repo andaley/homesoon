81b969538e20e17bd3dc518e7fb9651ea86d4e97
Noelle Daley <adriannenoelle@gmail.com>
2015-08-10 16:23:47 -0700

    added geolocation to home page

--- static/locate.js
+++ static/locate.js
@@ -1,7 +1,21 @@
-$('main-img')
-
-var map
 
 function initialize () {
-  
+  var mapOptions = {zoom: 15};
+  var map = new google.maps.Map(
+    document.getElementById('main-img'), mapOptions);
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        map.setCenter(pos);
+      }, function (){
+        handleNoGeolocation(true);
+      });
+  } else {
+    handleNoGeolocation(false);
+  }
 }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
