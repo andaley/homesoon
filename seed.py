7917b3329d6845d3a85d473515cd97f2f9fa1336
Noelle Daley <adriannenoelle@gmail.com>
2015-08-18 16:39:38 -0700

    fixed all img urls

--- seed.py
+++ seed.py
@@ -30,32 +30,36 @@ def load_posts(city_list):
             # If this object has a GeoCluster key, skip it, since it's not an actual post.
             if posting.get("GeoCluster"):
                 continue
-            else:
 
-                if not Posting.query.get(posting.get('PostingID')):
-                    # 0 Ask: 2400
-                    # 1 ImageThumb: 'http:\/\/images.craigslist.org...jpg'
-                    # 2 Latitude: 38.927686
-                    # 3 PostingTitle: 'title'
-                    # 4 PostedDate: '1438725510'
-                    # 5 Longitude: -122.3888
-                    # 6 PostingURL: '\/\/sfbay.craigslist.org...html'
-                    # 7 Bedrooms: '2'
-                    # 8 CategoryID: '1'
-                    # 9 PostingID: '5156767694'
 
-                    post_id = posting.get('PostingID')
-                    title = posting.get('PostingTitle')
-                    date_posted = posting.get('PostedDate')
-                    url = posting.get('PostingURL')
-                    img_url = posting.get('ImageThumb')
-                    price = posting.get('Ask')
-                    bedrooms = posting.get('Bedrooms')
-                    latitude = posting.get('Latitude')
-                    longitude = posting.get('Longitude')
+            if not Posting.query.get(posting.get('PostingID')):
+                # 0 Ask: 2400
+                # 1 ImageThumb: 'http:\/\/images.craigslist.org...jpg'
+                # 2 Latitude: 38.927686
+                # 3 PostingTitle: 'title'
+                # 4 PostedDate: '1438725510'
+                # 5 Longitude: -122.3888
+                # 6 PostingURL: '\/\/sfbay.craigslist.org...html'
+                # 7 Bedrooms: '2'
+                # 8 CategoryID: '1'
+                # 9 PostingID: '5156767694'
 
-                    new_post = Posting(post_id=post_id, title=title, date_posted=date_posted, url=url, img_url=img_url, price=price, bedrooms=bedrooms, latitude=latitude, longitude=longitude)
-                    db.session.add(new_post)
+                # Need to re-format img_url to embed in HTML.
+                raw_img_url = posting.get('ImageThumb')
+                if raw_img_url:
+                    new_url = raw_img_url.split(',')
+                    img_url = new_url[0] + '_300x300.jpg'
+
+                post_id = posting.get('PostingID')
+                title = posting.get('PostingTitle')
+                date_posted = posting.get('PostedDate')
+                url = posting.get('PostingURL')
+                price = posting.get('Ask')
+                bedrooms = posting.get('Bedrooms')
+                latitude = posting.get('Latitude')
+                longitude = posting.get('Longitude')
+                new_post = Posting(post_id=post_id, title=title, date_posted=date_posted, url=url, img_url=img_url, price=price, bedrooms=bedrooms, latitude=latitude, longitude=longitude)
+                db.session.add(new_post)
 
     db.session.commit()
 
